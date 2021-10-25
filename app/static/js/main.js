0edab8c312bc0bcd8d43b57ccc5463fa5988631a
JHigz <jhiguita@spartaglobal.com>
2021-10-25 12:34:13 +0100

    Text changes to index page

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,8 +1,8 @@
 function updateParagraph(){
   alert('This is the update paragraph section from the file')
-  document.getElementById("paragraph_field").innerHTML = "This the updated paragraph from the file"
+  document.getElementById("paragraph_field").innerHTML = "This is the updated paragraph from the file"
 }
 function updateParagraph2(){
   alert('This is the update paragraph section 2 from the file')
-  document.getElementById("paragraph_field2").innerHTML = "This the 2nd updated paragraph from the file"
+  document.getElementById("paragraph_field2").innerHTML = "This is the 2nd updated paragraph from the file"
 }
